4bb8f23eafe8b95bd7db4d887bb2bc2cecd00efb
createlex <createlex@192.168.1.3>
2018-06-08 13:10:26 -0700

    First Commot

---
+++ src/App01.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : App01.cpp
+// Author      : Alexander Kissi Jr
+// Version     :
+// Copyright   : Createlex
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "MyApp" << endl; // prints MyApp
+	return 0;
+}
