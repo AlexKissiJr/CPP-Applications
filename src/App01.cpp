18ee6c072999052f1cd766221805f5af89a86f48
createlex <createlex@192.168.1.3>
2018-06-08 13:22:46 -0700

    Added a for loop and vector array of strings.

--- src/App01.cpp
+++ src/App01.cpp
@@ -7,10 +7,25 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	cout << "MyApp" << endl; // prints MyApp
 	//Testing new Comment
+
+	vector<string> names;
+
+	names.push_back("Bob");
+	names.push_back("Apple");
+	names.push_back("Orange");
+
+
+	for (int i = 0; i < names.size(); i++){
+		cout << names[i] <<endl;
+	}
+
+
+
 	return 0;
 }
